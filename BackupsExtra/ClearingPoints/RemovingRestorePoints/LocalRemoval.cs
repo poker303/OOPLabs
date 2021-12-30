13cbeda83890c4ea7876f36887f67c86c41c4117
poker303 <varanous@gmail.com>
2021-12-30 18:31:22 +0300

    feat: add clearing

---
+++ BackupsExtra/ClearingPoints/RemovingRestorePoints/LocalRemoval.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Backups;
+
+namespace BackupsExtra.ClearingPointsAlgorithms
+{
+    public class LocalRemoval : IRestorePointRemoval
+    {
+        public void Delete(List<RestorePoint> deletedRestorePoints, ImprovedBackupJob backupJob)
+        {
+            foreach (RestorePoint restorePoint in backupJob.Points)
+            {
+                foreach (RestorePoint restorePointToDelete in deletedRestorePoints)
+                {
+                    if (restorePoint != restorePointToDelete) continue;
+                    var directory = new DirectoryInfo(Path.Combine(restorePointToDelete.Location, $"{restorePointToDelete.Name}{restorePointToDelete.Id}"));
+                    directory.Delete(true);
+                }
+            }
+
+            backupJob.DeleteRestorePoints(deletedRestorePoints);
+        }
+    }
+}
