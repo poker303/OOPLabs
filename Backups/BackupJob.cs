3fb4ca9b5dd12ded1dfdf76437baedba2628a6db
poker303 <varanous@gmail.com>
2021-12-30 18:37:53 +0300

    feat+refactor: changes + adding methods

--- Backups/BackupJob.cs
+++ Backups/BackupJob.cs
@@ -12,26 +12,26 @@ namespace Backups
         public BackupJob(string name, string location, IStorageSaving storage)
         {
             Name = name;
-            Path = location;
+            Location = location;
             _storage = storage;
             Points = new List<RestorePoint>();
         }
 
         public string Name { get; set; }
-        public string Path { get; set; }
+        public string Location { get; set; }
 
         public List<RestorePoint> Points { get; set; }
 
         public RestorePoint CreateRestorePoint(string restorePointName, IBackupSaving backupSaver, List<FileInfo> savedFiles, IFileSystem system)
         {
             _restorePointId++;
-            var restorePoint = new RestorePoint(restorePointName, _restorePointId, Path);
+            var restorePoint = new RestorePoint(restorePointName, _restorePointId, Location);
             _storage.SavingStorage(restorePoint, backupSaver, savedFiles, system);
             Points.Add(restorePoint);
             return restorePoint;
         }
 
-        public void AddJobObjects(IFileSystem system, List<FileInfo> files)
+        public virtual void AddJobObjects(IFileSystem system, List<FileInfo> files)
         {
             system.AddJobObjects(files);
         }
@@ -39,6 +39,14 @@ namespace Backups
         public void DeleteJobObjects(IFileSystem system, FileInfo file)
         {
             system.DeleteJobObject(file);
+        }
+
+        public void DeleteRestorePoints(List<RestorePoint> points)
+        {
+            foreach (RestorePoint point in points)
+            {
+                Points.Remove(point);
+            }
         }
     }
 }
